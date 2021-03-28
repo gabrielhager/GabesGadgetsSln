08a5d06ef52519e48b37593d6dda85e38e2433a4
Gabriel Hager <gabriel.a.hager@gmail.com>
2021-03-27 22:17:20 -0700

    Completed Lab1B steps 2-5

---
+++ GadgetEmporium/Models/IStoreRepository.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+namespace GadgetEmporium.Models
+{
+    public interface IStoreRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
---
+++ GadgetEmporium/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using GadgetEmporium.Models;
+namespace GadgetEmporium.Models.ViewModels
+{
+ public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
